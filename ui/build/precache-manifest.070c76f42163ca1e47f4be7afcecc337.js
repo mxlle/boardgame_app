c14536b35a4336feb06e95746c472d711ea14db6
Almut <almut.m.f@gmail.com>
2020-05-17 17:49:36 +0200

    Add new build

---
+++ ui/build/precache-manifest.070c76f42163ca1e47f4be7afcecc337.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1f48ee761a44aea6904b51d27578a937",
+    "url": "/index.html"
+  },
+  {
+    "revision": "4cf4a713788c5cdfdb3d",
+    "url": "/static/css/main.ac8d25c2.chunk.css"
+  },
+  {
+    "revision": "392381c7dfff9c02f3e0",
+    "url": "/static/js/2.63a499a4.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.63a499a4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4cf4a713788c5cdfdb3d",
+    "url": "/static/js/main.e4c8ce9d.chunk.js"
+  },
+  {
+    "revision": "e0fbf3c52eae81da3669",
+    "url": "/static/js/runtime-main.8b52ac59.js"
+  }
+]);
--- ui/build/service-worker.js
+++ ui/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.0439fa31d391a29d2b15850a104ee061.js"
+  "/precache-manifest.070c76f42163ca1e47f4be7afcecc337.js"
 );
 
 self.addEventListener('message', (event) => {
