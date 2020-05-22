7aff44ae37e0f402a7e8084ad9c474b3db444ea1
Owen Kieffer-Jones <okj@imos.net>
2020-05-23 00:23:52 +0200

    Built

--- ui/build/service-worker.js
+++ ui/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.615a8e24b69a65a346d94e4dc1e66eb5.js"
+  "/precache-manifest.34ed1d22ee7f019583ac25430639d958.js"
 );
 
 self.addEventListener('message', (event) => {
