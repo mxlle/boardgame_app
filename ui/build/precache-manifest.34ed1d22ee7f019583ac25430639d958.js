e475be8157f5353a91e3cfdf99a4b990663c2dba
Owen Kieffer-Jones <okj@imos.net>
2020-05-23 00:31:03 +0200

    Built

---
+++ ui/build/precache-manifest.34ed1d22ee7f019583ac25430639d958.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9d8f24602603fa1c1122e9f78f164dd",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b938914cc811ecb1ea1",
+    "url": "/static/css/main.9ae84c65.chunk.css"
+  },
+  {
+    "revision": "9b806deb7c2c42fe1999",
+    "url": "/static/js/2.26be37c6.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.26be37c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b938914cc811ecb1ea1",
+    "url": "/static/js/main.3d96ea7a.chunk.js"
+  },
+  {
+    "revision": "e0fbf3c52eae81da3669",
+    "url": "/static/js/runtime-main.8b52ac59.js"
+  },
+  {
+    "revision": "5e98861f6ef1ce225121abfb3f418166",
+    "url": "/static/media/getFetch.5e98861f.cjs"
+  }
+]);
