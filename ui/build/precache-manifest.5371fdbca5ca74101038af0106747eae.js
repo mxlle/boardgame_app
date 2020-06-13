0cd91f1eecbbf71f9f20a0da0d0eff028f99f1d6
Almut <almut.m.f@gmail.com>
2020-06-13 22:06:41 +0200

    Add missing ui build files

---
+++ ui/build/precache-manifest.5371fdbca5ca74101038af0106747eae.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7e20140acf1a51d1e46aece36e0aa82d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ffe33e4a707562ab762",
+    "url": "/static/css/main.a7d42794.chunk.css"
+  },
+  {
+    "revision": "84a3683e749a2874320b",
+    "url": "/static/js/2.85f94b9a.chunk.js"
+  },
+  {
+    "revision": "c134ff412a12ffc1280f654a8868980f",
+    "url": "/static/js/2.85f94b9a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6ffe33e4a707562ab762",
+    "url": "/static/js/main.1ca17f94.chunk.js"
+  },
+  {
+    "revision": "e0fbf3c52eae81da3669",
+    "url": "/static/js/runtime-main.8b52ac59.js"
+  },
+  {
+    "revision": "5e98861f6ef1ce225121abfb3f418166",
+    "url": "/static/media/getFetch.5e98861f.cjs"
+  }
+]);
